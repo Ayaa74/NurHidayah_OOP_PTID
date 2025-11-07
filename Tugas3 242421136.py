9a2bca49da3f9db1602c8ce506d53b90bee04151
Ayaa74 <trijuliyanti15075@gmail.com>
2025-11-07 16:43:37 +0800

    Add files via upload

---
+++ Tugas3 242421136.py
@@ -0,0 +1,72 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display_info(self):
+        return f"Name: {self.name}\nAge: {self.age}"
+
+class Lecturer(Person):
+    def __init__(self, name, age, nidn, course):
+        super().__init__(name, age)
+        self.nidn = nidn
+        self.course = course
+    
+    def teach(self):
+        return f"🎓 {self.name} is teaching {self.course}"
+    
+    def display_info(self):
+        base_info = super().display_info()
+        return f"{base_info}\nNIDN: {self.nidn}\nCourse: {self.course}"
+
+class Student(Person):
+    def __init__(self, name, age, nim, major):
+        super().__init__(name, age)
+        self.nim = nim
+        self.major = major
+    
+    def study(self):
+        return f"📚 {self.name} is studying in {self.major} major"
+    
+    def display_info(self):
+        base_info = super().display_info()
+        return f"{base_info}\nNIM: {self.nim}\nMajor: {self.major}"
+
+class UniversitySystem:
+    def __init__(self):
+        self.lecturers = []
+        self.students = []
+    
+    def add_lecturer(self, lecturer):
+        self.lecturers.append(lecturer)
+    
+    def add_student(self, student):
+        self.students.append(student)
+    
+    def display_all_data(self):
+        print("=" * 50)
+        print("🏫 UNIVERSITY UNDIKMA")
+        print("=" * 50)
+        
+        print("\n👨‍🏫 DATA ")
+        print("-" * 30)
+        for lecturer in self.lecturers:
+            print(lecturer.display_info())
+            print(lecturer.teach())
+            print("-" * 20)
+        
+        print("\n👨‍🎓 STUDENT DATA")
+        print("-" * 30)
+        for student in self.students:
+            print(student.display_info())
+            print(student.study())
+            print("-" * 20)
+
+lecturer1 = Lecturer("Edy", 17, "123456", "Object Oriented Programming")
+student1 = Student("Nur hidayah", 23, "24241136", "Teknologi Informasi")
+
+university = UniversitySystem()
+university.add_lecturer(lecturer1)
+university.add_student(student1)
+
+university.display_all_data()
